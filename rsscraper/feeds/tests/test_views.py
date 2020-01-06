b4c731ddfeeb196fa56b0fe955070adedefd4180
Alvaro Marquina <thesunno@gmail.com>
2020-01-05 20:03:34 -0400

    Testing Feed deletion

--- rsscraper/feeds/tests/test_views.py
+++ rsscraper/feeds/tests/test_views.py
@@ -1,9 +1,10 @@
 import pytest
 from django.conf import settings
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.test import RequestFactory, Client
 from django.http import Http404
 
+from rsscraper.feeds.models import Feed
 from rsscraper.feeds.views import FeedDetailView, FeedItemDetailView,\
     FeedDeleteView
 from rsscraper.feeds.tests.factories import FeedFactory
@@ -88,11 +89,22 @@ class TestFeedDeleteView:
 
         assert response.status_code == 404
 
+        # getting deletion confimation message
         feed = FeedFactory(user=user)
         response = client.get(
             reverse('feeds:delete', kwargs={'pk': feed.pk}))
 
         assert response.status_code == 200
+        text = f'Are you sure you want to delete feed "{feed.title}"?'
+        assert text in response.content.decode()
+
+        response = client.post(
+            reverse('feeds:delete', kwargs={'pk': feed.pk}),
+        )
+
+        assert response.status_code == 302
+        assert resolve(response.url).view_name == 'users:detail'
+        assert not Feed.objects.filter(pk=feed.pk).exists()
 
 
 class TestFeedItemDetailView:
